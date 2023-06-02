36fb795bc0446174357943914843c0fabb6fc0c1
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-06-02 23:35:57 +0200

    Some updates in system

--- first_app/lib/screens/index.dart
+++ first_app/lib/screens/index.dart
@@ -5,6 +5,10 @@ class indexScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contact'),   
+      ),
+    );
   }
 }
