ec0a297f8b070aaceb8dde9808dca3705b1d964e
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-06-09 15:28:20 +0200

    Some updates in allTaskScreen

--- first_app/lib/screens/allTasksScreen.dart
+++ first_app/lib/screens/allTasksScreen.dart
@@ -1,26 +1,50 @@
-
-// ignore_for_file: prefer_const_constructors
-
-// ignore: use_key_in_widget_constructors
 import 'package:flutter/material.dart';
 
-class AllTasksScreen extends StatelessWidget {
+class AllTasksScreen extends StatefulWidget {
+  @override
+  _AllTasksScreenState createState() => _AllTasksScreenState();
+}
+
+class _AllTasksScreenState extends State<AllTasksScreen> {
+  int _currentIndex = 0;
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Color.fromARGB(255, 235, 235, 235),
-          title: Text('Todas as Tarefas',
-          style: TextStyle(
-            color: Colors.black,
-          ),),
+          title: Text(
+            'Emma - App',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
           elevation: 0,
+          centerTitle: false,
         ),
         drawer: Drawer(
           backgroundColor: Colors.amber,
           child: ListView(
             children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                ),
+                child: Text(
+                  'Título do Drawer',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
               ListTile(
                 title: Text('Todas as Tarefas'),
                 onTap: () {
@@ -33,8 +57,46 @@ class AllTasksScreen extends StatelessWidget {
             ],
           ),
         ),
-        body: Container(
-          color: Color.fromARGB(255, 235, 235, 235),
+        body: IndexedStack(
+          index: _currentIndex,
+          children: [
+            Container(
+              color: Color.fromARGB(255, 235, 235, 235),
+              child: Center(
+                child: Text('Página 1'),
+              ),
+            ),
+            Container(
+              color: Color.fromARGB(255, 235, 235, 235),
+              child: Center(
+                child: Text('Página 2'),
+              ),
+            ),
+            Container(
+              color: Color.fromARGB(255, 235, 235, 235),
+              child: Center(
+                child: Text('Página 3'),
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: _onTabTapped,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Página 1',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Página 2',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Página 3',
+            ),
+          ],
         ),
       ),
     );
