d6f31c9918274a0ed7bbd8847758c74d86dcff8c
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-06-12 12:44:56 +0200

    Some updates in Profile.dart

--- first_app/lib/screens/Profile.dart
+++ first_app/lib/screens/Profile.dart
@@ -1,16 +1,55 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({super.key});
+  const Profile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Container (
+    return Container(
       color: Color.fromARGB(255, 235, 235, 235),
       child: Container(
         width: double.infinity,
-        height: 350,
+        height: 400,
         color: Colors.amber,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+                image: DecorationImage(
+                  image: AssetImage('assets/homem.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 50,
+              child: Column(
+                children: [
+                  Text(
+                    'Mauro Peniel',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Software Engineer',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
